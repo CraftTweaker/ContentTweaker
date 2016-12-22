ed5e708e0d1a213f2bb55ddc596de259dd6e23a8
Skylar Sommers <ssommers@criticalmix.com>
2016-12-22 13:03:51 -0500

    Added JsonRequired Annotation

---
+++ src/main/java/com/teamacronymcoders/tailoredobjects/deserializer/JsonRequired.java
@@ -0,0 +1,8 @@
+package com.teamacronymcoders.tailoredobjects.deserializer;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JsonRequired {
+}
