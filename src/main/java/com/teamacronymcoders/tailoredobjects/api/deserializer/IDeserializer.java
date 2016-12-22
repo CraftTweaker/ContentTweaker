0b19b187089a6ecde41e9a51e0983cfa622d1a7e
Skylar Sommers <ssommers@criticalmix.com>
2016-12-22 12:48:19 -0500

    Altered the deserializer

--- src/main/java/com/teamacronymcoders/tailoredobjects/api/deserializer/IDeserializer.java
+++ src/main/java/com/teamacronymcoders/tailoredobjects/api/deserializer/IDeserializer.java
@@ -1,17 +1,22 @@
 package com.teamacronymcoders.tailoredobjects.api.deserializer;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
+import javax.annotation.Nonnull;
 
 public interface IDeserializer {
     /*
      * @returns serializer's name. Must be unique.
      */
+    @Nonnull
     String getName();
 
     /*
-     * @param jsonObject a json representation of an object that uses this serializer
+     * @param gson Gson object that can be used to deserialize
+     * @param jsonObject a json representation of an object that uses this deserializer
      *
      * @returns a boolean representing if the object successfully deserialized
      */
-    boolean deserializeObject(JsonObject jsonObject);
+    boolean deserializeObject(@Nonnull Gson gson, @Nonnull JsonObject jsonObject);
 }
--- src/main/java/com/teamacronymcoders/tailoredobjects/deserializer/Deserializer.java
+++ src/main/java/com/teamacronymcoders/tailoredobjects/deserializer/Deserializer.java
@@ -2,8 +2,10 @@ package com.teamacronymcoders.tailoredobjects.deserializer;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.teamacronymcoders.base.util.logging.ILogger;
 import com.teamacronymcoders.tailoredobjects.TailoredObjects;
 import com.teamacronymcoders.tailoredobjects.api.TailoredObjectsAPI;
+import com.teamacronymcoders.tailoredobjects.api.deserializer.DeserializerRegistry;
 import com.teamacronymcoders.tailoredobjects.api.deserializer.IDeserializer;
 import com.teamacronymcoders.tailoredobjects.representations.ObjectRepresentation;
 import org.apache.commons.io.FileUtils;
@@ -13,17 +15,18 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Deserializer {
     public Gson fileGson;
     public File file;
-    public Map<String, IDeserializer> deserializers;
+    public DeserializerRegistry deserializerRegistry;
+    public ILogger logger;
 
     public Deserializer(File file) {
         this.fileGson = new Gson();
         this.file = file;
-        this.deserializers = TailoredObjectsAPI.getInstance().getDeserializerRegistry().getDeserializers();
+        this.logger = TailoredObjects.instance.getLogger();
+        this.deserializerRegistry = TailoredObjectsAPI.getInstance().getDeserializerRegistry();
     }
 
     public void deserialize() {
@@ -42,16 +45,19 @@ public class Deserializer {
                 Type listType = new TypeToken<List<ObjectRepresentation>>(){}.getType();
                 List<ObjectRepresentation> objectRepresentations = this.fileGson.fromJson(jsonString, listType);
                 objectRepresentations.forEach(objectRepresentation -> {
-                    IDeserializer deserializer = this.deserializers.get(objectRepresentation.getType());
-                    boolean successful = deserializer.deserializeObject(objectRepresentation.getObject());
-                    if(!successful) {
-                        TailoredObjects.instance.getLogger().error("Object " + objectRepresentation.getName() +
-                                " failed to deserialize");
+                    IDeserializer deserializer = this.deserializerRegistry.getDeserializer(objectRepresentation.getType());
+                    if(deserializer != null) {
+                        boolean success = deserializer.deserializeObject(this.fileGson, objectRepresentation.getObject());
+                        if(!success) {
+                            logger.error("Object " + objectRepresentation.getName() + " failed to deserialize");
+                        }
+                    } else {
+                        logger.error("No deserializer type " + objectRepresentation.getName() + " found for " +
+                                objectRepresentation.getName());
                     }
                 });
             } catch (IOException e) {
-                TailoredObjects.instance.getLogger().getLogger().error("File " + file.getName() +
-                        " failed to deserialize", e);
+                logger.getLogger().error("File " + file.getName() + "failed to deserialize", e);
             }
         }
     }
