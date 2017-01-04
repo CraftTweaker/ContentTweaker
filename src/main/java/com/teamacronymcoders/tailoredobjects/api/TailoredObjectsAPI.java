238de6ba46053caab10e59cb6ae69afb73fc9dfa
Skylar Sommers <ssommers@criticalmix.com>
2017-01-04 18:13:07 -0500

    Added ItemStackDeserializer

--- src/main/java/com/teamacronymcoders/tailoredobjects/api/TailoredObjectsAPI.java
+++ src/main/java/com/teamacronymcoders/tailoredobjects/api/TailoredObjectsAPI.java
@@ -1,13 +1,21 @@
 package com.teamacronymcoders.tailoredobjects.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.teamacronymcoders.tailoredobjects.api.deserializer.DeserializerRegistry;
+import com.teamacronymcoders.tailoredobjects.api.json.ItemStackDeserializer;
+import net.minecraft.item.ItemStack;
 
 public class TailoredObjectsAPI {
     private static TailoredObjectsAPI instance;
     private DeserializerRegistry deserializerRegistry;
+    private Gson gson;
 
     private TailoredObjectsAPI() {
         this.deserializerRegistry = new DeserializerRegistry();
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(ItemStack.class, new ItemStackDeserializer());
+        gson = builder.create();
     }
 
     public static TailoredObjectsAPI getInstance() {
@@ -19,5 +27,9 @@ public class TailoredObjectsAPI {
 
     public DeserializerRegistry getDeserializerRegistry() {
         return this.deserializerRegistry;
+    }
+
+    public Gson getGson() {
+        return this.gson;
     }
 }
