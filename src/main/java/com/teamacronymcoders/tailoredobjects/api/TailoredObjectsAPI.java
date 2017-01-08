f48faf18c579edfa6fb75feab52e109f35d1e5db
Skylar Sommers <ssommers@criticalmix.com>
2017-01-08 01:40:46 -0500

    CreativeTabs now uses CreativeTabs List

--- src/main/java/com/teamacronymcoders/tailoredobjects/api/TailoredObjectsAPI.java
+++ src/main/java/com/teamacronymcoders/tailoredobjects/api/TailoredObjectsAPI.java
@@ -1,6 +1,7 @@
 package com.teamacronymcoders.tailoredobjects.api;
 
 import com.teamacronymcoders.tailoredobjects.api.deserializer.DeserializerRegistry;
+import com.teamacronymcoders.tailoredobjects.api.utils.CreativeTabsResourceList;
 import com.teamacronymcoders.tailoredobjects.api.utils.ResourceList;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -17,7 +18,7 @@ public class TailoredObjectsAPI {
         this.deserializerRegistry = new DeserializerRegistry();
         this.materialResourceList = new ResourceList<>(Material.class);
         this.soundTypeResourceList = new ResourceList<>(SoundType.class);
-        this.creativeTabsResourceList = new ResourceList<>(CreativeTabs.class);
+        this.creativeTabsResourceList = new CreativeTabsResourceList();
     }
 
     public static TailoredObjectsAPI getInstance() {
