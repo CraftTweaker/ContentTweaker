3209b7b67a8e8e4358b8ff74eb36761876a8eaad
Skylar Sommers <ssommers@criticalmix.com>
2017-02-11 20:04:17 -0500

    Removed unnecessary Import

--- src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/VanillaDeserializers.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/VanillaDeserializers.java
@@ -3,7 +3,6 @@ package com.teamacronymcoders.contenttweaker.modules.vanilla;
 import com.teamacronymcoders.contenttweaker.api.deserializer.DeserializerBase;
 import com.teamacronymcoders.contenttweaker.api.deserializer.RegisterDeserializerEvent;
 import com.teamacronymcoders.contenttweaker.modules.vanilla.blocks.BlockRepresentation;
-import com.teamacronymcoders.contenttweaker.modules.vanilla.items.CreativeTabContent;
 import com.teamacronymcoders.contenttweaker.modules.vanilla.items.CreativeTabRepresentation;
 import com.teamacronymcoders.contenttweaker.modules.vanilla.items.ItemRepresentation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
