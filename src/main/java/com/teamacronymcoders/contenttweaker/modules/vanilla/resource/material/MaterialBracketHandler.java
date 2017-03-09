416338c7d1ba3c6d20060e68507f99228c4d157e
Skylar Sommers <ssommers@criticalmix.com>
2017-03-09 10:05:22 -0500

    Finally found the issue with the bracket handler

--- src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/resource/material/MaterialBracketHandler.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/resource/material/MaterialBracketHandler.java
@@ -2,13 +2,19 @@ package com.teamacronymcoders.contenttweaker.modules.vanilla.resource.material;
 
 import com.teamacronymcoders.contenttweaker.api.ContentTweakerAPI;
 import com.teamacronymcoders.contenttweaker.modules.vanilla.resource.ResourceBracketHandler;
+import minetweaker.MineTweakerAPI;
+import net.minecraft.block.material.Material;
 
 public class MaterialBracketHandler extends ResourceBracketHandler {
     public MaterialBracketHandler() {
         super("Material", MaterialBracketHandler.class);
     }
 
-    public IMaterialDefinition getMaterial(String name) {
-        return new MaterialDefinition(ContentTweakerAPI.getInstance().getBlockMaterials().getResource(name));
+    public static IMaterialDefinition getMaterial(String name) {
+        Material material = ContentTweakerAPI.getInstance().getBlockMaterials().getResource(name);
+        if(material == null) {
+            MineTweakerAPI.logError("Could not find Material for name: " + name);
+        }
+        return new MaterialDefinition(material);
     }
 }
