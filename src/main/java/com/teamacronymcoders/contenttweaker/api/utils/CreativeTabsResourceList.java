0983581209a8626e31c4b4cc3b7cc0ffc81226c4
Skylar Sommers <ssommers@criticalmix.com>
2017-04-09 19:22:35 -0400

    Removed Logging

--- src/main/java/com/teamacronymcoders/contenttweaker/api/utils/CreativeTabsResourceList.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/api/utils/CreativeTabsResourceList.java
@@ -1,6 +1,5 @@
 package com.teamacronymcoders.contenttweaker.api.utils;
 
-import com.teamacronymcoders.contenttweaker.ContentTweaker;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
@@ -25,8 +24,6 @@ public class CreativeTabsResourceList extends ResourceList<CreativeTabs> {
         while (creativeTabsIterable.hasNext() && resourceCreativeTab == null) {
             CreativeTabs creativeTab = creativeTabsIterable.next();
             String tabLabel = ReflectionHelper.getPrivateValue(CreativeTabs.class, creativeTab, "tabLabel", "field_78034_o");
-            //TODO REMOVE
-            ContentTweaker.instance.getLogger().warning(tabLabel);
             if (name.equalsIgnoreCase(tabLabel)) {
                 resourceCreativeTab = creativeTab;
             }
