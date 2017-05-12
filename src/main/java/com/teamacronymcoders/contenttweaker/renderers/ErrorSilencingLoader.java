22d730e971ee94f91d871412434f09411a9735a7
Skylar Sommers <ssommers@criticalmix.com>
2017-05-12 00:12:10 -0400

    Silence warnings coming from ErrorSilencingLoader

--- src/main/java/com/teamacronymcoders/contenttweaker/renderers/ErrorSilencingLoader.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/renderers/ErrorSilencingLoader.java
@@ -1,15 +1,19 @@
 package com.teamacronymcoders.contenttweaker.renderers;
 
+import com.teamacronymcoders.base.client.models.wrapped.WrappedModelLoader;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 
 import static com.teamacronymcoders.contenttweaker.ContentTweaker.MOD_ID;
 
+@SideOnly(Side.CLIENT)
 public class ErrorSilencingLoader implements ICustomModelLoader {
     private boolean firstReload = true;
     private boolean createdResourcePack = false;
@@ -20,7 +24,8 @@ public class ErrorSilencingLoader implements ICustomModelLoader {
 
     @Override
     public boolean accepts(ResourceLocation modelLocation) {
-        return !createdResourcePack && modelLocation.getResourceDomain().equals(MOD_ID);
+        return !createdResourcePack && modelLocation.getResourceDomain().equals(MOD_ID) &&
+                !WrappedModelLoader.getInstance().accepts(modelLocation);
     }
 
     @Override
