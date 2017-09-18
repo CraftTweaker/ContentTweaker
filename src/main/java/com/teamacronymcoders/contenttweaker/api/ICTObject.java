93990d7322c0e35a28cf39d201527ce10d04b98c
SkySom <skylar.som@gmail.com>
2017-09-18 14:23:56 -0400

    Should fix to string stuff that Blood was talking about.

--- src/main/java/com/teamacronymcoders/contenttweaker/api/ICTObject.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/api/ICTObject.java
@@ -2,4 +2,8 @@ package com.teamacronymcoders.contenttweaker.api;
 
 public interface ICTObject<T> {
     T getInternal();
+
+    default String getString() {
+        return this.getInternal().toString();
+    }
 }
