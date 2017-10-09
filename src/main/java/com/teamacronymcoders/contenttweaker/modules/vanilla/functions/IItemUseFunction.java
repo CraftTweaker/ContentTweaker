41cfabff0b6592b498c8ae72f08e6859272122bf
SkySom <skylar.som@gmail.com>
2017-10-09 09:18:10 -0400

    Added Import

--- src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/functions/IItemUseFunction.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/functions/IItemUseFunction.java
@@ -3,6 +3,7 @@ package com.teamacronymcoders.contenttweaker.modules.vanilla.functions;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.enums.ActionResult;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.enums.Facing;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.blockpos.IBlockPos;
+import com.teamacronymcoders.contenttweaker.api.ctobjects.enums.Hand;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.world.IWorld;
 import crafttweaker.api.player.IPlayer;
 
