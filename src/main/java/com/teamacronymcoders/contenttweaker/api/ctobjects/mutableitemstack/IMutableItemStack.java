dde1301264ca3a356519af1a8d7cf02a7dc86fde
Skylar Sommers <skylar.som@gmail.com>
2017-10-11 11:54:54 -0400

    Added ZenMethod

--- src/main/java/com/teamacronymcoders/contenttweaker/api/ctobjects/mutableitemstack/IMutableItemStack.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/api/ctobjects/mutableitemstack/IMutableItemStack.java
@@ -4,15 +4,20 @@ import com.teamacronymcoders.contenttweaker.api.ctobjects.entity.player.ICTPlaye
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenRegister
 @ZenClass("mods.contenttweaker.MutableItemStack")
 public interface IMutableItemStack extends IItemStack {
+    @ZenMethod
     void setCount(int count);
 
+    @ZenMethod
     void shrink(int count);
 
+    @ZenMethod
     void grow(int count);
 
+    @ZenMethod
     void damage(int amount, ICTPlayer player);
 }
