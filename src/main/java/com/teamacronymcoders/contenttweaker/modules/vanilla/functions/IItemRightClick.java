45629e6bfb6ab7327348ed4b6320f42610577fc5
SkySom <skylar.som@gmail.com>
2018-03-06 23:11:17 -0500

    Fix IITemRightClick using wrong type

--- src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/functions/IItemRightClick.java
+++ src/main/java/com/teamacronymcoders/contenttweaker/modules/vanilla/functions/IItemRightClick.java
@@ -2,11 +2,12 @@ package com.teamacronymcoders.contenttweaker.modules.vanilla.functions;
 
 import com.teamacronymcoders.contenttweaker.api.ctobjects.entity.player.ICTPlayer;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.enums.Hand;
+import com.teamacronymcoders.contenttweaker.api.ctobjects.mutableitemstack.IMutableItemStack;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.mutableitemstack.MCMutableItemStack;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.world.IWorld;
 import stanhebben.zenscript.annotations.ZenClass;
 
 @ZenClass("mods.contenttweaker.IItemRightClick")
 public interface IItemRightClick {
-    String onRightClick(MCMutableItemStack itemStack, IWorld world, ICTPlayer player, String hand);
+    String onRightClick(IMutableItemStack itemStack, IWorld world, ICTPlayer player, String hand);
 }
