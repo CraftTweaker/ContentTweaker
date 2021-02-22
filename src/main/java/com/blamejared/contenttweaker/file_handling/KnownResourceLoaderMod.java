5795236cab7879163bde6a0ea930e14ce0e1f28a
youyihj <youyi580@qq.com>
2021-02-22 22:16:58 +0800

    GLOBAL_DATA_AND_RESOURCE_PACKS support

--- src/main/java/com/blamejared/contenttweaker/file_handling/KnownResourceLoaderMod.java
+++ src/main/java/com/blamejared/contenttweaker/file_handling/KnownResourceLoaderMod.java
@@ -4,7 +4,8 @@ import java.io.*;
 
 public enum KnownResourceLoaderMod {
     THE_LOADER("theloader", "the_loader/resourcepacks/contenttweaker", "the_loader/datapacks/contenttweaker"),
-    OPEN_LOADER("openloader", "openloader/resources/contenttweaker", "openloader/data/contenttweaker");
+    OPEN_LOADER("openloader", "openloader/resources/contenttweaker", "openloader/data/contenttweaker"),
+    GLOBAL_DATA_AND_RESOURCE_PACKS("globaldataandresourcepacks", "global_resource_packs/contenttweaker", "global_data_packs/contenttweaker");
     
     final String modid;
     final File resourcePackDirectory;
