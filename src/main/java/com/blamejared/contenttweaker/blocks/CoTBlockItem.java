f2be14eef1c1ec7a3c834dd344b92d447d573c72
youyihj <youyi580@qq.com>
2021-02-28 12:50:47 +0800

    block item functions

--- src/main/java/com/blamejared/contenttweaker/blocks/CoTBlockItem.java
+++ src/main/java/com/blamejared/contenttweaker/blocks/CoTBlockItem.java
@@ -5,8 +5,11 @@ import com.blamejared.contenttweaker.api.blocks.*;
 import com.blamejared.contenttweaker.api.functions.*;
 import com.blamejared.contenttweaker.api.items.*;
 import com.blamejared.contenttweaker.api.resources.*;
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.impl.item.MCItemStackMutable;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
@@ -20,8 +23,6 @@ public class CoTBlockItem extends BlockItem implements IIsCotItem {
         super(blockIn.getBlock(), builder);
         this.setRegistryName(blockIn.getRegistryNameNonNull());
     }
-
-    public IItemInventoryTick itemInventoryTick;
     
     @Nonnull
     @Override
@@ -42,10 +43,49 @@ public class CoTBlockItem extends BlockItem implements IIsCotItem {
 
     @Override
     public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-        if (itemInventoryTick == null) {
-            super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
-        } else {
-            itemInventoryTick.apply(new MCItemStackMutable(stack), worldIn, entityIn, itemSlot, isSelected);
-        }
+        VanillaFactory.REGISTRY.getFunction(this, IItemInventoryTick.class)
+                .map(iItemInventoryTick -> {
+                    iItemInventoryTick.apply(new MCItemStackMutable(stack), worldIn, entityIn, itemSlot, isSelected);
+                    return 0;
+                })
+                .orElseGet(() -> {
+                    super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+                    return 0;
+                });
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        return VanillaFactory.REGISTRY.getFunction(this, IItemRightClick.class)
+                .map(iItemRightClick -> {
+                    ItemStack stack = playerIn.getHeldItem(handIn);
+                    switch (iItemRightClick.apply(new MCItemStackMutable(stack), playerIn, worldIn, handIn.name())) {
+                        case "SUCCESS":
+                            return ActionResult.resultSuccess(stack);
+                        case "PASS":
+                            return ActionResult.resultPass(stack);
+                        case "FAIL":
+                            return ActionResult.resultFail(stack);
+                        case "CONSUME":
+                            return ActionResult.resultConsume(stack);
+                        default:
+                            CraftTweakerAPI.logWarning("invalid action result type! Set PASS by default.");
+                            return ActionResult.resultPass(stack);
+                    }
+                }).orElseGet(() -> super.onItemRightClick(worldIn, playerIn, handIn));
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        return VanillaFactory.REGISTRY.getFunction(this, IItemHitEntity.class)
+                .map(iItemHitEntity -> iItemHitEntity.apply(new MCItemStackMutable(stack), target, attacker))
+                .orElseGet(() -> super.hitEntity(stack, target, attacker));
+    }
+
+    @Override
+    public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
+        return VanillaFactory.REGISTRY.getFunction(this, IItemInteractWithEntity.class)
+                .map(iItemInteractWithEntity -> ActionResultType.valueOf(iItemInteractWithEntity.apply(new MCItemStackMutable(stack), playerIn, target, hand.name())))
+                .orElseGet(() -> super.itemInteractionForEntity(stack, playerIn, target, hand));
     }
 }
