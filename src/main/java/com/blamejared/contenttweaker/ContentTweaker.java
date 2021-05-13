f5e5e38eba2c16b220cbb3f0c83a4981a78f0004
youyihj <youyi580@qq.com>
2021-05-13 22:45:50 +0800

    remove deprecated example collection event

--- src/main/java/com/blamejared/contenttweaker/ContentTweaker.java
+++ src/main/java/com/blamejared/contenttweaker/ContentTweaker.java
@@ -2,10 +2,7 @@ package com.blamejared.contenttweaker;
 
 import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.ScriptLoadingOptions;
-import com.blamejared.crafttweaker.impl.commands.script_examples.ExampleCollectionEvent;
 import net.minecraft.block.Block;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
@@ -24,12 +21,6 @@ public class ContentTweaker {
     public ContentTweaker() {
         VanillaFactory.generateStuffForMyModId(MOD_ID);
         FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Block.class, EventPriority.LOW, this::registerItems);
-        MinecraftForge.EVENT_BUS.addListener(this::addExampleScriptFiles);
-    }
-
-    private void addExampleScriptFiles(ExampleCollectionEvent event) {
-        event.addResource(new ResourceLocation(MOD_ID, "contenttweaker_simple_walkthrough"));
-        event.addResource(new ResourceLocation(MOD_ID, "contenttweaker_advanced_function"));
     }
     
     /**
