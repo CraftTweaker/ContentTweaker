1ccb36ccd0e824584b15891c78188817246697c5
youyihj <youyi580@qq.com>
2021-05-21 15:54:38 +0800

    ActionQueueFluidForRegistration#validate

--- src/main/java/com/blamejared/contenttweaker/actions/ActionQueueFluidForRegistration.java
+++ src/main/java/com/blamejared/contenttweaker/actions/ActionQueueFluidForRegistration.java
@@ -1,8 +1,13 @@
 package com.blamejared.contenttweaker.actions;
 
+import com.blamejared.contenttweaker.VanillaFactory;
 import com.blamejared.contenttweaker.api.CoTRegistry;
 import com.blamejared.contenttweaker.api.fluids.IIsCotFluid;
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.actions.IAction;
+import com.blamejared.crafttweaker.api.logger.ILogger;
+import com.blamejared.crafttweaker.api.zencode.impl.loaders.LoaderActions;
+import org.openzen.zencode.shared.CodePosition;
 
 public class ActionQueueFluidForRegistration implements IAction {
     private final IIsCotFluid fluid;
@@ -16,6 +21,19 @@ public class ActionQueueFluidForRegistration implements IAction {
     @Override
     public void apply() {
         registry.addFluid(fluid);
+    }
+
+    @Override
+    public boolean validate(ILogger logger) {
+        if(VanillaFactory.isRegisterAllowed()) {
+            return true;
+        }
+
+        final LoaderActions loaderActions = CraftTweakerAPI.getCurrentRun().getLoaderActions();
+        final CodePosition declaredScriptPosition = getDeclaredScriptPosition();
+        final String format = "Cannot register Item '%s' since it was called too late. Registering must be done during '#loader contenttweaker', but file %s is loaded in '#loader %s'!";
+        logger.error(String.format(format, fluid.getRegistryName(), declaredScriptPosition.getFilename(), loaderActions.getLoaderName()));
+        return false;
     }
 
     @Override
