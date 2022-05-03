2ab08c31e8c8d91f69a0f2066b86d2a32e0c1666
TheSilkMiner <thesilkminer@outlook.com>
2022-05-03 11:04:09 +0200

    Fix Unknown being registered on the wrong loader
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/contenttweaker/core/zen/rt/Unknown.java
+++ Common/src/main/java/com/blamejared/contenttweaker/core/zen/rt/Unknown.java
@@ -1,5 +1,6 @@
 package com.blamejared.contenttweaker.core.zen.rt;
 
+import com.blamejared.contenttweaker.core.api.ContentTweakerConstants;
 import com.blamejared.contenttweaker.core.api.object.ObjectFactory;
 import com.blamejared.contenttweaker.core.api.object.ObjectType;
 import com.blamejared.contenttweaker.core.zen.ContentTweakerZenConstants;
@@ -7,10 +8,10 @@ import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import org.openzen.zencode.java.ZenCodeType;
 
 @ZenCodeType.Name(ContentTweakerZenConstants.UNKNOWN_CLASS_MARKER)
-@ZenRegister
+@ZenRegister(loaders = ContentTweakerConstants.CONTENT_LOADER_ID)
 public final class Unknown {
     @ZenCodeType.Name(ContentTweakerZenConstants.UNKNOWN_FACTORY_CLASS_MARKER)
-    @ZenRegister
+    @ZenRegister(loaders = ContentTweakerConstants.CONTENT_LOADER_ID)
     public static final class Factory implements ObjectFactory<Unknown> {
         public static final Factory INSTANCE = new Factory();
 
