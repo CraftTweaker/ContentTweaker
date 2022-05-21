ef51045b6890d17a8310e4de6497b463cd411300
TheSilkMiner <thesilkminer@outlook.com>
2022-05-21 15:57:45 +0200

    Provide factory method to create object holders
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- Common/src/main/java/com/blamejared/contenttweaker/core/api/object/ObjectHolder.java
+++ Common/src/main/java/com/blamejared/contenttweaker/core/api/object/ObjectHolder.java
@@ -2,9 +2,32 @@ package com.blamejared.contenttweaker.core.api.object;
 
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public interface ObjectHolder<T> extends Supplier<T> {
+    static <T> ObjectHolder<T> of(final ObjectType<T> type, final ResourceLocation id, final Supplier<T> objectCreator) {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(objectCreator);
+        return new ObjectHolder<>() {
+            @Override
+            public ObjectType<T> type() {
+                return type;
+            }
+
+            @Override
+            public ResourceLocation id() {
+                return id;
+            }
+
+            @Override
+            public T get() {
+                return objectCreator.get();
+            }
+        };
+    }
+
     ObjectType<T> type();
     ResourceLocation id();
     @Override T get();
