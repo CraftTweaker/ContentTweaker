7340ffe590181018a7dd596b7a9f8d1c4e79d8bb
TheSilkMiner <thesilkminer@outlook.com>
2022-05-21 16:09:26 +0200

    Add forgotten natives
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ Common/src/main/java/com/blamejared/contenttweaker/vanilla/zen/rt/ContentTweakerBasicNatives.java
@@ -0,0 +1,28 @@
+package com.blamejared.contenttweaker.vanilla.zen.rt;
+
+import com.blamejared.contenttweaker.core.api.ContentTweakerConstants;
+import com.blamejared.contenttweaker.vanilla.api.zen.ContentTweakerVanillaConstants;
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.BracketEnum;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+
+public final class ContentTweakerBasicNatives {
+
+    @NativeTypeRegistration(value = Item.class, zenCodeName = ContentTweakerVanillaConstants.VANILLA_NATIVE_PACKAGE + ".Item")
+    @ZenRegister(loaders = ContentTweakerConstants.CONTENT_LOADER_ID)
+    public static final class ItemNative {
+        private ItemNative() {}
+    }
+
+    @BracketEnum("minecraft:item/rarity")
+    @NativeTypeRegistration(value = Rarity.class, zenCodeName = ContentTweakerVanillaConstants.VANILLA_NATIVE_PACKAGE + ".ItemRarity")
+    @ZenRegister(loaders = ContentTweakerConstants.CONTENT_LOADER_ID)
+    public static final class RarityNative {
+        private RarityNative() {}
+    }
+
+    private ContentTweakerBasicNatives() {}
+}
+
